b42877b4ad3d2927325508e1c40ca91ded3c142e
Joe-noh <goflb.jh@gmail.com>
2021-06-12 17:04:30 +0900

    use navigo router

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 import 'ress'
+
+import Navigo from 'navigo'
 import basicGeometries from './src/pages/basic-geometries'
 
-basicGeometries()
+const router = new Navigo('/threes')
+
+router.on('/basic-geometries', basicGeometries)
+
+router.resolve()
