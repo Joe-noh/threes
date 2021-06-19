d1f17e74bfa4ac904ebd15fa660ca68bdb5703d5
Joe-noh <goflb.jh@gmail.com>
2021-06-19 12:32:51 +0900

    first babylon mesh

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Engine, Scene } from 'react-babylonjs'
+import { Vector3, Color3 } from '@babylonjs/core'
 
 function App() {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+    setHeight(window.innerHeight)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Engine antialias adaptToDeviceRatio canvasId='babylon' width={width} height={height} data-testid="babylon">
+      <Scene>
+        <arcRotateCamera name="camera" target={Vector3.Zero()} alpha={Math.PI / 3} beta={Math.PI / 4} radius={10} />
+        <hemisphericLight name='light' direction={Vector3.Up()} />
+        <box name='box' position={Vector3.Zero()}>
+          <standardMaterial name='boxMaterial' diffuseColor={Color3.FromHexString('#669130')} />
+        </box>
+      </Scene>
+    </Engine>
+  )
 }
 
-export default App;
+export default App
