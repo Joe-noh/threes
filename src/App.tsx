2e64e8f75daced94bac32da89d3e89c4f4d5b358
Joe-noh <goflb.jh@gmail.com>
2021-06-20 17:36:39 +0900

    built a room

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Index from './pages/index'
 import BasicGeometries from './pages/basic-geometries'
 import Suzanne from './pages/suzanne'
+import Clock from './pages/clock'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" exact component={Index} />
       <Route path="/basic-geometries" component={BasicGeometries} />
       <Route path="/suzanne" component={Suzanne} />
+      <Route path="/clock" component={Clock} />
     </Switch>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ export default function Index() {
   return (
     <ul style={{ margin: '2rem' }}>
       <li><Link to="/basic-geometries">Basic Geometries</Link></li>
+      <li><Link to="/suzanne">Suzanne</Link></li>
+      <li><Link to="/clock">Clock</Link></li>
     </ul>
   )
 }
