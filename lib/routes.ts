e64d9051f17b2d352033505a36ee13aa5814c097
Joe-noh <goflb.jh@gmail.com>
2021-06-28 20:04:39 +0900

    create index page

---
+++ lib/routes.ts
@@ -0,0 +1,22 @@
+function toKebab(str: string) {
+  return str.split('').map((char, index) => {
+    if (char.toUpperCase() === char) {
+      return `${index !== 0 ? '-' : ''}${char.toLowerCase()}`
+    } else {
+      return char
+    }
+  }).join('');
+}
+
+const labels = [
+  'BasicGeometries',
+  'Suzanne',
+  'Clock',
+  'Bone',
+  'Can',
+  'Cloth'
+]
+
+export const routes = labels.map(label => {
+  return { path: `./${toKebab(label)}`, label }
+})
--- pages/index.ts
+++ pages/index.ts
@@ -1,1 +1,16 @@
-export {}
+import { routes } from '../lib/routes'
+
+const ul = document.createElement('ul')
+const canvas = document.querySelector('canvas')!
+
+routes.forEach(route => {
+  const li = document.createElement('li')
+  const a = document.createElement('a')
+
+  a.href = `./${route.path}.html`
+  a.textContent = route.label
+
+  ul.appendChild(li).appendChild(a)
+})
+
+document.body.replaceChild(ul, canvas)
